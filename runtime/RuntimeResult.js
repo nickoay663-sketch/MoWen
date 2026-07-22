6b4ce964d1f4754947f2ad0e97afd49d5b0a029f
nickoay663-sketch <nickoay663@gmail.com>
2026-07-23 01:02:16 +0200

    Add RuntimeResult v1.0
    Create standard Runtime result object.

---
+++ runtime/RuntimeResult.js
@@ -0,0 +1,23 @@
+class RuntimeResult {
+
+    constructor(){
+
+        this.recognition = null;
+
+        this.definition = null;
+
+        this.evidence = null;
+
+        this.correspondence = null;
+
+        this.reasoning = null;
+
+        this.responsibility = null;
+
+        this.generator = null;
+
+    }
+
+}
+
+export default RuntimeResult;
