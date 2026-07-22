19441b02d9333cba4b8bd88a17c71d5e031acdd2
nickoay663-sketch <nickoay663@gmail.com>
2026-07-23 01:12:23 +0200

    Add RuntimeVersion v1.0
    Create Runtime version information.

---
+++ runtime/RuntimeVersion.js
@@ -0,0 +1,15 @@
+const RuntimeVersion = {
+
+    name: "MoWen Runtime",
+
+    version: "0.1.0",
+
+    protocol: "Honest Runtime",
+
+    author: "MoWen",
+
+    status: "Development"
+
+};
+
+export default RuntimeVersion;
