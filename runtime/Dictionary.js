618825239b7de198837ef810c9836af79a165a72
nickoay663-sketch <nickoay663@gmail.com>
2026-07-22 20:07:33 +0200

    Add Runtime Dictionary v1.0
    Create Runtime Dictionary.

---
+++ runtime/Dictionary.js
@@ -0,0 +1,25 @@
+const Dictionary = {
+
+    objects: [
+        "我",
+        "你",
+        "他",
+        "她",
+        "我们",
+        "他们"
+    ],
+
+    concepts: [
+        "老师",
+        "医生",
+        "学生",
+        "父亲",
+        "母亲",
+        "国家",
+        "政府",
+        "法律"
+    ]
+
+};
+
+export default Dictionary;
