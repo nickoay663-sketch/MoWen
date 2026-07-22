73ce87dff47f158a06ca81902b4995f4846cee86
nickoay663-sketch <nickoay663@gmail.com>
2026-07-22 20:01:37 +0200

    Update RecognitionEngine v3.0
    Support multiple object and concept recognition.

--- runtime/RecognitionEngine.js
+++ runtime/RecognitionEngine.js
@@ -9,18 +9,40 @@ class RecognitionEngine {
         const result = {
             originalText: this.text,
             objects: [],
-            concepts: [],
-            definitions: [],
-            evidences: []
+            concepts: []
         };
 
-        if(this.text.includes("我")){
-            result.objects.push("我");
-        }
+        const objectWords = [
+            "我",
+            "你",
+            "他",
+            "她",
+            "我们",
+            "他们"
+        ];
 
-        if(this.text.includes("老师")){
-            result.concepts.push("老师");
-        }
+        const conceptWords = [
+            "老师",
+            "医生",
+            "学生",
+            "父亲",
+            "母亲",
+            "国家",
+            "政府",
+            "法律"
+        ];
+
+        objectWords.forEach(word=>{
+            if(this.text.includes(word)){
+                result.objects.push(word);
+            }
+        });
+
+        conceptWords.forEach(word=>{
+            if(this.text.includes(word)){
+                result.concepts.push(word);
+            }
+        });
 
         return result;
 
