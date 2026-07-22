a7c99d6b2e64ea8c9356c1a4ebc6ce8a297b7791
nickoay663-sketch <nickoay663@gmail.com>
2026-07-22 11:53:26 +0200

    Update RecognitionEngine v1.1
    Prepare Recognition Engine runtime structure.

--- runtime/RecognitionEngine.js
+++ runtime/RecognitionEngine.js
@@ -16,11 +16,7 @@ class RecognitionEngine {
 
             definitions: [],
 
-            evidences: [],
-
-            reasoning: [],
-
-            conclusions: []
+            evidences: []
 
         };
 
