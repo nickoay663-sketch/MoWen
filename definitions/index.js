f4ba68743b8e0926208e88a240ce99dd79023423
nickoay663-sketch <nickoay663@gmail.com>
2026-07-22 20:14:51 +0200

    Add Definition Index v1.0
    Create definition mapping.

---
+++ definitions/index.js
@@ -0,0 +1,21 @@
+const Definitions = {
+
+    "老师":"Teacher.md",
+
+    "医生":"Doctor.md",
+
+    "学生":"Student.md",
+
+    "父亲":"Father.md",
+
+    "母亲":"Mother.md",
+
+    "国家":"Country.md",
+
+    "政府":"Government.md",
+
+    "法律":"Law.md"
+
+};
+
+export default Definitions;
