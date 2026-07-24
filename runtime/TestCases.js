977cdfe7d54707f812fa6ee04c30461ca17d3294
nickoay663-sketch <nickoay663@gmail.com>
2026-07-24 02:46:24 +0200

    Add TestCases v1.0

---
+++ runtime/TestCases.js
@@ -0,0 +1,26 @@
+const TestCases = [
+
+    {
+        name: "Basic Statement Test",
+
+        input: "我是老师。",
+
+        expectedQuestion:
+            "该证词中的对象是否被明确定义？"
+
+    },
+
+    {
+        name: "Concept Test",
+
+        input: "文明带来了进步。",
+
+        expectedQuestion:
+            "该证词中的对象是否被明确定义？"
+
+    }
+
+];
+
+
+export default TestCases;
