21deaa7f19101d1a92597df7205c4c27494a00c8
nickoay663-sketch <nickoay663@gmail.com>
2026-07-24 16:10:57 +0200

    Upgrade Runtime Entry v0.2

--- run.js
+++ run.js
@@ -1,24 +1,31 @@
 import HonestRuntime from "./runtime/HonestRuntime.js";
 import ReportFormatter from "./runtime/ReportFormatter.js";
 
-
 const input =
     process.argv.slice(2).join(" ")
     || "我是老师。";
 
-
 const runtime =
     new HonestRuntime(input);
-
 
 const result =
     runtime.run();
 
-
 const report =
     new ReportFormatter(result).run();
 
+// Runtime Information
+report.runtime = {
 
+    name: "MoWen Honest Runtime",
+
+    version: "0.2",
+
+    principle: "没有定义，就没有推理。"
+
+};
+
+// Output
 console.log(
     JSON.stringify(report, null, 4)
 );
