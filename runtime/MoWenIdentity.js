53bd77652007a5ed17599e3a4347725417aa96b7
nickoay663-sketch <nickoay663@gmail.com>
2026-07-27 04:18:51 +0200

    Fix MoWenIdentity config reference
    Updated MoWenIdentity.js to use 'principles' instead of 'principle' and removed the 'rule' property.

--- runtime/MoWenIdentity.js
+++ runtime/MoWenIdentity.js
@@ -12,15 +12,11 @@ class MoWenIdentity {
 
 
             principle:
-                MoWenConfig.principle,
+                MoWenConfig.principles,
 
 
             mission:
                 "检查证词、定义对象、验证对应、追问责任。",
-
-
-            rule:
-                MoWenConfig.rule,
 
 
             status:
