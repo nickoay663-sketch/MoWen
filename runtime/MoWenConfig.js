f7716749418588a0e5205043d2cb5e45b00a73f5
nickoay663-sketch <nickoay663@gmail.com>
2026-07-27 13:20:46 +0200

    Upgrade MoWenConfig v2.0

--- runtime/MoWenConfig.js
+++ runtime/MoWenConfig.js
@@ -1,10 +1,13 @@
 const MoWenConfig = {
 
-    version: "0.3.1",
+    version: "2.0",
 
     name: "MoWen",
 
     motto: "诚实运行",
+
+    mission:
+        "检查证词、定义对象、验证对应、追问责任。",
 
     principles: {
 
@@ -15,7 +18,10 @@ const MoWenConfig = {
             "没有证词，就没有结论。",
 
         responsibility:
-            "没有责任，就没有文明。"
+            "没有责任，就没有文明。",
+
+        reconstruction:
+            "不能承担的表达，应当重构。"
 
     },
 
