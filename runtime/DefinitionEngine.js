ade4d3685cea96abddb7849b2a6a5f1d5aaeddc2
nickoay663-sketch <nickoay663@gmail.com>
2026-07-29 19:05:06 +0200

    Upgrade DefinitionEngine v2.2 with Semantic Object Definition Flow

--- runtime/DefinitionEngine.js
+++ runtime/DefinitionEngine.js
@@ -1,58 +1,124 @@
 import MoWenConfig from "./MoWenConfig.js";
 import Definitions from "../definitions/index.js";
 
+
 class DefinitionEngine {
 
-    constructor(testimony) {
 
-        this.testimony = testimony || "";
+    constructor(semanticObject) {
+
+        this.semanticObject = semanticObject || {};
 
     }
+
+
 
     run() {
 
-        const definitions = this.findDefinitions();
+
+        const definitions =
+            this.findDefinitions();
+
+
 
         return {
 
-            testimony: this.testimony,
+
+            semanticObject:
+
+                this.semanticObject,
+
+
 
             principle:
+
                 MoWenConfig.principles.definition,
+
+
 
             definitions,
 
-            matched: definitions.length > 0,
 
-            question:
+
+            status:
+
                 definitions.length > 0
-                    ? null
-                    : "该证词中的对象是否被明确定义？"
+
+                    ? "definition-available"
+
+                    : "need-definition-verification",
+
+
+
+            questions:
+
+                definitions.length > 0
+
+                    ? []
+
+                    :
+
+                    [
+
+                        "该表达中的概念是否已经明确定义？"
+
+                    ],
+
+
+
+            version:
+
+                "2.2"
+
 
         };
 
+
     }
+
+
 
     findDefinitions() {
 
-        return Object.keys(Definitions)
+
+        const concepts =
+
+            this.semanticObject.concepts || [];
+
+
+
+        return concepts
 
             .filter(concept =>
 
-                this.testimony.includes(concept)
+                Definitions[concept]
 
             )
 
             .map(concept => ({
 
+
                 concept,
 
-                definition: Definitions[concept]
+
+                definition:
+
+                    Definitions[concept],
+
+
+                source:
+
+                    "MoWen Definition Library"
+
 
             }));
 
+
     }
 
+
 }
+
+
 
 export default DefinitionEngine;
