d8f44ee9db2f7604d0165e52fabc4fa5621735e5
nickoay663-sketch <nickoay663@gmail.com>
2026-07-29 21:37:20 +0200

    Support Spanish Definitions in DefinitionEngine

--- runtime/DefinitionEngine.js
+++ runtime/DefinitionEngine.js
@@ -1,9 +1,8 @@
 import MoWenConfig from "./MoWenConfig.js";
 import Definitions from "../definitions/index.js";
-
+import SpanishDefinitions from "../languages/es-ES/Definitions.js";
 
 class DefinitionEngine {
-
 
     constructor(semanticObject) {
 
@@ -11,114 +10,79 @@ class DefinitionEngine {
 
     }
 
-
-
     run() {
-
 
         const definitions =
             this.findDefinitions();
 
-
-
         return {
 
-
             semanticObject:
-
                 this.semanticObject,
 
-
-
             principle:
-
                 MoWenConfig.principles.definition,
-
-
 
             definitions,
 
-
-
             status:
-
                 definitions.length > 0
-
                     ? "definition-available"
-
                     : "need-definition-verification",
 
-
-
             questions:
-
                 definitions.length > 0
-
                     ? []
-
-                    :
-
-                    [
-
+                    : [
                         "该表达中的概念是否已经明确定义？"
-
                     ],
 
-
-
             version:
-
-                "2.2"
-
+                "2.4"
 
         };
 
+    }
+
+    getDefinitions() {
+
+        return this.semanticObject.language === "es-ES"
+            ? SpanishDefinitions
+            : Definitions;
 
     }
 
-
-
     findDefinitions() {
 
+        const library =
+            this.getDefinitions();
 
         const concepts =
-
             this.semanticObject.concepts || [];
-
-
 
         return concepts
 
             .filter(concept =>
 
-                Definitions[concept]
+                library[concept.word || concept]
 
             )
 
             .map(concept => ({
 
-
-                concept,
-
+                concept:
+                    concept.word || concept,
 
                 definition:
-
-                    Definitions[concept],
-
+                    library[concept.word || concept],
 
                 source:
-
                     "MoWen Definition Library"
-
 
             }));
 
-
     }
 
-
 }
-
-
 
 export default DefinitionEngine;
